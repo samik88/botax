7760852e2d4d325a6e06af05a70558ae135b9649
Mehdi Ahmadov <mehdi.ahmedov@gmail.com>
2017-11-04 16:31:43 -0700

    testing dashbot

--- messages/index.js
+++ messages/index.js
@@ -7,7 +7,7 @@ https://aka.ms/abs-node-waterfall
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
-// var pdfFiller = require('pdffiller')
+var pdfFiller = require('pdffiller')
 var useEmulator = (process.env.NODE_ENV == 'development');
 const dashbot = require('dashbot')('W8wcbHLZu8ECDLKqhPBfKRTf5prKYBtI7p3oQAn8').generic;
 
@@ -53,7 +53,7 @@ bot.dialog('/', [
                 "whateverJson": "any JSON specific to your platform can be stored here"
             }
         };
-        dashbot.logIncoming(messageForDashbot);
+        dashbot.logOutgoing(messageForDashbot);
         builder.Prompts.text(session, "How many people are in your party?");
     },
 
