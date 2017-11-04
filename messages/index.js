ea8171b227fb14ab9db3501ac618827c72bcac15
Mehdi Ahmadov <mehdi.ahmedov@gmail.com>
2017-11-04 14:01:38 -0700

    added salam

--- messages/index.js
+++ messages/index.js
@@ -22,7 +22,7 @@ bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+        builder.Prompts.text(session, "Salam... What's your name?");
     },
     function (session, results) {
         session.userData.name = results.response;
