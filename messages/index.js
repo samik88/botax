d6617a266b4665565b74b016eee079ce3d9b1fda
Samira <Allahverdiyeva>
2017-11-05 02:27:59 -0800

    added axios

--- messages/index.js
+++ messages/index.js
@@ -7,7 +7,7 @@ https://aka.ms/abs-node-waterfall
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
-var http = require('http');
+var axios = require('axios');
 // var pdfFiller = require('pdffiller');
 var useEmulator = (process.env.NODE_ENV == 'development');
 var dashbot = require('dashbot')('W8wcbHLZu8ECDLKqhPBfKRTf5prKYBtI7p3oQAn8').generic;
