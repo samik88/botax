820bb2c9632303f1c3150a2100415abfcfb40f42
Samira <Allahverdiyeva>
2017-11-05 00:05:51 -0700

    test prompt

--- messages/index.js
+++ messages/index.js
@@ -55,7 +55,7 @@ bot.dialog('/', [
         var message = "Is your name is different than on SSN?";
         var options = "yes |no";
         logOutgoingMessage(message);
-        builder.Prompts.choice(session, message, "yes|no", {
+        builder.Prompts.choice(session, message, promptChoices, {
             listStyle: builder.ListStyle.button
         });
 
