48878c8c03183867e44d9057894620be4430f769
Samira <Allahverdiyeva>
2017-11-05 10:09:32 -0800

    removed pdffiller

--- messages/index.js
+++ messages/index.js
@@ -8,7 +8,6 @@ var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
 var axios = require('axios');
-// var pdfFiller = require('pdffiller');
 var useEmulator = (process.env.NODE_ENV == 'development');
 var dashbot = require('dashbot')('W8wcbHLZu8ECDLKqhPBfKRTf5prKYBtI7p3oQAn8').generic;
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
